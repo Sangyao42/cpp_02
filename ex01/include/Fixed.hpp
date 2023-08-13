9e451f9a3a9f141cea22bb3690574402e4afdf23
Sangyao42 <wangsangyao@hotmail.com>
2023-08-13 15:41:58 +0200

    Updated[ex01]: Fixed.cpp with all member functions

--- ex01/include/Fixed.hpp
+++ ex01/include/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: sawang <sawang@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/11 22:46:31 by sawang            #+#    #+#             */
-/*   Updated: 2023/08/11 22:58:22 by sawang           ###   ########.fr       */
+/*   Updated: 2023/08/13 14:43:31 by sawang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,15 +22,18 @@ class Fixed
 		Fixed(const int value);
 		Fixed(const float value);
 		Fixed(const Fixed &src);
-		Fixed &operator=(Fixed const &rhs);
+		Fixed &operator=(const Fixed &rhs);
 		~Fixed();
 
-		int getRawBits(void) const;
-		void setRawBits(int const raw);
+		int		getRawBits(void) const;
+		void	setRawBits(int const raw);
+
+		float	toFloat(void) const;
+		int		toInt(void) const;
 
 	private:
-		int _fixedPointValue;
-		static const int _fractionalBits = 8;
+		int					_fixedPointValue;
+		static const int	_fractionalBits = 8;
 };
 
 std::ostream &operator<<(std::ostream &o, Fixed const &rhs);
--- ex01/src/Fixed.cpp
+++ ex01/src/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: sawang <sawang@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/11 22:55:32 by sawang            #+#    #+#             */
-/*   Updated: 2023/08/11 23:02:20 by sawang           ###   ########.fr       */
+/*   Updated: 2023/08/13 15:06:30 by sawang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,56 @@ Fixed::Fixed() : _fixedPointValue(0)
 Fixed::Fixed(const int value)
 {
 	std::cout << "Int constructor called" << std::endl;
+	this->_fixedPointValue = value << this->_fractionalBits;
 };
 
+Fixed::Fixed(const float value)
+{
+	std::cout << "Float constructor called" << std::endl;
+	this->_fixedPointValue = (int)roundf(value * (1 << this->_fractionalBits));
+};
+
+Fixed::Fixed(const Fixed &src)
+{
+	std::cout << "Copy constructor called" <<std::endl;
+	*this = src; //copy assignment called
+};
+
+Fixed &Fixed::operator=(const Fixed &rhs)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	if (this != &rhs)
+		this->_fixedPointValue = rhs.getRawBits();
+	return (*this);
+};
+
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called" << std::endl;
+};
+
+int		Fixed::getRawBits(void) const
+{
+	return (this->_fixedPointValue);
+};
+
+void Fixed::setRawBits(int const raw)
+{
+	this->_fixedPointValue = raw;
+};
+
+float Fixed::toFloat(void) const
+{
+	return ((float)this->_fixedPointValue / (float)(1 << this->_fractionalBits));
+};
+
+int Fixed::toInt(void) const
+{
+	return (this->_fixedPointValue >> this->_fractionalBits);
+};
+
+std::ostream &operator<<(std::ostream &o, Fixed const &rhs)
+{
+	o << rhs.toFloat();
+	return (o);
+};
