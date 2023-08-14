3d41d342f77c093af7b7c58d01af8576f97d2e35
Sangyao42 <wangsangyao@hotmail.com>
2023-08-14 16:25:38 +0200

    Cleaned[ex00]: Fixed.cpp

--- ex00/src/Fixed.cpp
+++ ex00/src/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: sawang <sawang@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/11 22:19:53 by sawang            #+#    #+#             */
-/*   Updated: 2023/08/11 22:29:24 by sawang           ###   ########.fr       */
+/*   Updated: 2023/08/14 16:25:14 by sawang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,13 @@
 Fixed::Fixed() : _fixedPointValue(0)
 {
 	std::cout << "Default constructor called" << std::endl;
-};
+}
 
 Fixed::Fixed(const Fixed &src)
 {
 	std::cout << "Copy constructor called" <<std::endl;
 	*this = src;
-};
+}
 
 Fixed &Fixed::operator=(const Fixed &rhs)
 {
@@ -30,20 +30,20 @@ Fixed &Fixed::operator=(const Fixed &rhs)
 	if (this != &rhs)
 		this->_fixedPointValue = rhs.getRawBits();
 	return (*this);
-};
+}
 
 Fixed::~Fixed()
 {
 	std::cout << "Destructor called" << std::endl;
-};
+}
 
 int		Fixed::getRawBits(void) const
 {
 	std::cout << "getRawBits member function called" << std::endl;
 	return (this->_fixedPointValue);
-};
+}
 
 void	Fixed::setRawBits(int const raw)
 {
 	this->_fixedPointValue = raw;
-};
+}
