e0de670865576319dd43403dc75a5eb53692ff8a
Sangyao42 <wangsangyao@hotmail.com>
2023-08-14 16:24:21 +0200

    Finished[ex01]: understanding how conversion between fixed-point and float-point works

--- ex01/src/Fixed.cpp
+++ ex01/src/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: sawang <sawang@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/11 22:55:32 by sawang            #+#    #+#             */
-/*   Updated: 2023/08/13 15:06:30 by sawang           ###   ########.fr       */
+/*   Updated: 2023/08/14 15:42:35 by sawang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,25 +17,25 @@
 Fixed::Fixed() : _fixedPointValue(0)
 {
 	std::cout << "Default constructor called" << std::endl;
-};
+}
 
 Fixed::Fixed(const int value)
 {
 	std::cout << "Int constructor called" << std::endl;
 	this->_fixedPointValue = value << this->_fractionalBits;
-};
+}
 
 Fixed::Fixed(const float value)
 {
 	std::cout << "Float constructor called" << std::endl;
 	this->_fixedPointValue = (int)roundf(value * (1 << this->_fractionalBits));
-};
+}
 
 Fixed::Fixed(const Fixed &src)
 {
 	std::cout << "Copy constructor called" <<std::endl;
 	*this = src; //copy assignment called
-};
+}
 
 Fixed &Fixed::operator=(const Fixed &rhs)
 {
@@ -43,12 +43,12 @@ Fixed &Fixed::operator=(const Fixed &rhs)
 	if (this != &rhs)
 		this->_fixedPointValue = rhs.getRawBits();
 	return (*this);
-};
+}
 
 Fixed::~Fixed()
 {
 	std::cout << "Destructor called" << std::endl;
-};
+}
 
 int		Fixed::getRawBits(void) const
 {
@@ -58,20 +58,20 @@ int		Fixed::getRawBits(void) const
 void Fixed::setRawBits(int const raw)
 {
 	this->_fixedPointValue = raw;
-};
+}
 
 float Fixed::toFloat(void) const
 {
 	return ((float)this->_fixedPointValue / (float)(1 << this->_fractionalBits));
-};
+}
 
 int Fixed::toInt(void) const
 {
 	return (this->_fixedPointValue >> this->_fractionalBits);
-};
+}
 
 std::ostream &operator<<(std::ostream &o, Fixed const &rhs)
 {
 	o << rhs.toFloat();
 	return (o);
-};
+}
