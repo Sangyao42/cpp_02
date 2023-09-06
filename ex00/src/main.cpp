eb5654deaf68dfca47a7af3f5538a157a5a35378
Sangyao42 <wangsangyao@hotmail.com>
2023-09-06 17:45:05 +0200

    Cleaned[ex00]

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: sawang <sawang@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/11 21:22:09 by sawang            #+#    #+#             */
-/*   Updated: 2023/08/11 22:30:40 by sawang           ###   ########.fr       */
+/*   Updated: 2023/09/06 17:44:49 by sawang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,9 @@ int	main( void )
 	Fixed c;
 
 	c = b;
-	std::cout << a.getRawBits() << std::endl; std::cout << b.getRawBits() << std::endl; std::cout << c.getRawBits() << std::endl;
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
 
 	return 0;
 }
